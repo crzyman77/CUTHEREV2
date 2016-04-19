4845f1bfb7cee8c9eda8e484ea392d00edeb7002
Chris Gillis <cgillis93@gmail.com>
2016-04-19 19:36:51 -0400

    Changes To post to DB

--- model/extraCreditAjax.php
+++ model/extraCreditAjax.php
@@ -1,17 +1,24 @@
 <?php
 require_once '../model/model.php';
-   error_reporting(0); // Needed put in for LocalHost
+  // error_reporting(0); // Needed put in for LocalHost
 
 $selectedClassArray = json_decode(stripslashes($_POST['class']),true);
 //class_number: tempRes[0], class_section: tempRes[1], instructor_id: tempRes[2], event_id: event, student_email: email
+print_r($selectedClassArray);
 foreach ($selectedClassArray as $class){
    // print_r($class);
     $classNum =$class[class_number];
+    print_r($classNum);
     $classSec =$class[class_section];
-    $className = $class[class_name];
+    print_r($classSec);
     $instId =$class[instructor_id];
+    print_r($instId);
     $eventId =$class[event_id];
+    print_r($eventId);
     $email = $class[student_email];
-    addToClassList($classNum, $classSec,$className, $instId, $eventId, $email);
+    print_r($email);
+   $result = addToClassList($classNum, $classSec, $instId, $eventId, $email);
+   print_r($result);
+   
 }
 ?>
