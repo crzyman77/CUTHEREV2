f3b7270e6d426afa8ede1a92192d77fc494477c1
Chris Gillis <cgillis93@gmail.com>
2016-04-22 13:28:50 -0400

    Random Changes

--- model/model.php
+++ model/model.php
@@ -258,7 +258,7 @@ if(!isset($_SESSION))
                 $query = "SELECT \n"
                             . "	`class_number`, \n"
                             . "	`class_section`, \n"
-                            . "	`inst_id`, \n"
+                            . "	`instructor_id`, \n"
                             . "	`event_id`, \n"
                             . "	`student_email` \n"
                             . "FROM \n"
@@ -463,7 +463,7 @@ if(!isset($_SESSION))
         // Extra Credit Lists
         function addToClassList($class_number, $class_section, $instructor_id, $event_id, $student_email){
             $db = getDBConnection();
-            $query = "INSERT INTO extra_credit_list (`class_number`, `class_section`, `inst_id`, `event_id`,`student_email`) \n"
+            $query = "INSERT INTO extra_credit_list (`class_number`, `class_section`, `instructor_id`, `event_id`,`student_email`) \n"
                     . "VALUES (:class_number, :class_sec, :inst_id, :ev_id, :email)";
             $statement = $db -> prepare ($query);
             $statement->bindValue (':class_number',$class_number);
