57181f6508087e98c3f2cb6e3b6f526b5ec51549
Nate Meyer <shadenath@gmail.com>
2016-04-23 14:06:34 -0400

    Moar CSS
    The CSS, it is endless

--- view/footerInclude.php
+++ view/footerInclude.php
@@ -2,17 +2,17 @@
         <section class="footer-info">
             <div class="container">
                 <br />
-                <a href="http://clarion.edu/" class="logo">
+                <a href="http://clarion.edu/" class="logo col-md-4 hidden-sm hidden-xs">
                     <strong>Clarion</strong>
                     <br />
                     <strong>University</strong>
                 </a>
-                <div>
+                <div class="col-md-4">
                     <p><a href="http://a.cms.clarion.edu/10?skin=oucampus&amp;account=clarion&amp;site=www&amp;action=de&amp;path=/index.pcf" id="direct-edit">© </a> Clarion University of Pennsylvania <br> GPS Address: <a href="https://www.google.com/maps/place/909+E+Wood+St,+Clarion,+PA+16214/@41.2108797,-79.3794223,17z/data=!3m1!4b1!4m2!3m1!1s0x89ccccca95cc9d17:0x292a07243852113b" target="_blank">909 E Wood St. Clarion PA, 16214</a> <br> 
                     Mailing Address: 840 Wood St. Clarion PA, 16214 <br>
                     <a href="#">800-672-7171</a> or <a>814-393-2000</a></p>
                 </div>
-                <div id="footer-logos">
+                <div id="footer-logos" class="col-md-4">
                     <a class="passhe" title="get connected to PASSHE" href="http://www.passhe.edu/" target="_blank"> <img src="../images/sshe-logo.jpg" alt="Get Connected to PASSHE" height="48" width="150"></a> <a class="college-portrait" title="College Portrait" href="http://www.collegeportraits.org/PA/Clarion" target="_blank"><img src="../images/button-PASSHE-college-portrait.jpg" alt="College Portrait" height="49" width="150"></a>
                 </div>
             </div>
